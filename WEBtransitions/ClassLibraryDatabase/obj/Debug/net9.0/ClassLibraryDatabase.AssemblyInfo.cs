3b297b6c0bd05511cd50f0d6ce35364fc93fbd97
Gediminas Bukauskas <gbukauskas@gmail.com>
2025-05-07 09:48:32 +0300

    Pager is ready

--- WEBtransitions/ClassLibraryDatabase/obj/Debug/net9.0/ClassLibraryDatabase.AssemblyInfo.cs
+++ WEBtransitions/ClassLibraryDatabase/obj/Debug/net9.0/ClassLibraryDatabase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibraryDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4615dbbe68aa0af762bc9d819e47125746655451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84169219a5b589e774e12d257c6f90f0d4e21d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibraryDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibraryDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
