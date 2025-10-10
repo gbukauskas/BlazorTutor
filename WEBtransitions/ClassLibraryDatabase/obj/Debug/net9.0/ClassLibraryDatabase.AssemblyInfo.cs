6a2bd7991b13613354813afe297bfe5d26168524
Gediminas Bukauskas <gbukauskas@gmail.com>
2025-10-10 17:14:42 +0300

    Page size was corrected for the 'Employees' component.

--- WEBtransitions/ClassLibraryDatabase/obj/Debug/net9.0/ClassLibraryDatabase.AssemblyInfo.cs
+++ WEBtransitions/ClassLibraryDatabase/obj/Debug/net9.0/ClassLibraryDatabase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibraryDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a136129f6b9196b176f10289bd32a0e8c4cd5ae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6233094940cb82a3d3fe48786cbdfd271516a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibraryDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibraryDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
